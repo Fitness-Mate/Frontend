ee9ff56e7c5321b211bc638765e75e4ab7d536a7
최훈오 <gnsdh86166@gmail.com>
2025-01-04 23:49:51 +0900

    [Chore 🚀] clone 폴더 삭제

--- clone/clone/README.md
+++
@@ -1,7 +0,0 @@
-# Fitmate
-
-## 테스트 계정
-
-id : gnsdh8616@konkuk.ac.kr
-
-password: 123123gg
--- clone/clone/clone/clone/clone/.husky/console-log-check.sh
+++
@@ -1,5 +0,0 @@
-if git diff --cached --name-only | xargs grep -i 'console\.log' --with-filename --line-number;
-then
-    echo -e "\n${red}COMMIT REJECTED!  Please remove console."
-    exit 1;
-fi
