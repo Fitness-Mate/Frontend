ccda6ec2ab210e6f2f9ab0958c4b78cd64540bf2
강민정 <staccato189@gmail.com>
2025-01-05 22:18:56 +0900

    [Fix 🔨] dnd 개선을 위해 sortable-list-tsc로 루틴 목록 dragdrop 편집 시도

--- src/components/Modal/components/Routine/RoutineFixItem.tsx
+++ src/components/Modal/components/Routine/RoutineFixItem.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import { UseFormRegister, UseFormSetValue } from "react-hook-form"
 
-import { DraggableProvided } from "@hello-pangea/dnd"
-
 import Icon from "@components/Icon/Icon"
 import Input from "@components/Input/Input"
 
@@ -11,50 +9,48 @@ import { useModal } from "@hooks/useModal"
 import * as S from "./StyledRoutineModal"
 
 interface RoutineItemProps {
-  routine: { routineId: number; routineName: string }
+  routine: {
+    routineId: number
+    routineName: string
+    routineIndex: number
+  }
   index: number
   register: UseFormRegister<{
-    routines: { routineId: number; routineName: string; routineIndex: number }[]
+    routines: {
+      routineId: number
+      routineName: string
+      routineIndex: number
+    }[]
   }>
-  provided: DraggableProvided
   setValue: UseFormSetValue<{
-    routines: { routineId: number; routineName: string; routineIndex: number }[]
+    routines: {
+      routineId: number
+      routineName: string
+      routineIndex: number
+    }[]
   }>
+  startDrag: (index: number) => void
 }
 
 const RoutineItem: React.FC<RoutineItemProps> = ({
   routine,
   index,
   register,
-  provided,
   setValue,
 }) => {
   const { onOpen } = useModal("삭제")
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // input 값이 수정될 때마다 해당 루틴을 업데이트
-    const updatedRoutine = {
-      ...routine,
-      routineName: e.target.value,
-    }
-
-    // setValue로 수정된 루틴 이름을 반영
-    setValue(`routines.${index}.routineName`, updatedRoutine.routineName)
-    console.log(updatedRoutine)
+    setValue(`routines.${index}.routineName`, e.target.value)
   }
 
   const handleDeleteClick = () => {
-    // 삭제 모달을 연다
     onOpen()
   }
 
   return (
-    <S.RoutineItemContainer
-      ref={(el) => {
-        provided.innerRef(el)
-      }}
-      {...provided.draggableProps}>
-      <S.HandleIconButtonWrapper {...provided.dragHandleProps}>
+    <S.RoutineItemContainer>
+      <S.HandleIconButtonWrapper>
         <Icon
           icon="Handle"
           size={18}
@@ -62,12 +58,12 @@ const RoutineItem: React.FC<RoutineItemProps> = ({
       </S.HandleIconButtonWrapper>
       <S.RoutineHoverArea>
         <S.InputWrapper>
-          <Input key={routine.routineId}>
+          <Input>
             <Input.Input
               props={{
                 ...register(`routines.${index}.routineName`),
-                value: routine.routineName, // 현재 값 표시
-                onChange: handleInputChange, // 수정 시 업데이트
+                value: routine.routineName,
+                onChange: handleInputChange,
               }}
             />
           </Input>
--- src/components/Modal/components/Routine/RoutineFixModal.tsx
+++ src/components/Modal/components/Routine/RoutineFixModal.tsx
@@ -1,13 +1,7 @@
 import { useEffect } from "react"
 import { useForm } from "react-hook-form"
 
-import {
-  DragDropContext,
-  Draggable,
-  DraggableProvidedDraggableProps,
-  DropResult,
-  Droppable,
-} from "@hello-pangea/dnd"
+import SortableList from "@badahertz52/sortable-list-tsc/dist/SortableList"
 
 import Button from "@components/Button/Button"
 import Modal from "@components/Modal/Modal"
@@ -51,58 +45,9 @@ const RoutineFixModal = () => {
   }, [routines, setValue])
   const watchedRoutines = watch("routines")
 
-  const handleDrop = (droppedItem: DropResult) => {
-    if (!droppedItem.destination) return
-
-    const updatedRoutines: MyRoutines[] = [...watchedRoutines]
-
-    const [movedRoutine] = updatedRoutines.splice(droppedItem.source.index, 1)
-    updatedRoutines.splice(droppedItem.destination.index, 0, movedRoutine)
-
-    updatedRoutines.forEach((routine, index) => {
-      routine.routineIndex = index + 1
-    })
-
-    setValue("routines", updatedRoutines)
-  }
-
   const onSubmit = (data: { routines: MyRoutines[] }) => {
     mutate({ routines: data.routines })
     onClose()
-  }
-
-  const getItemStyle = (
-    style?: DraggableProvidedDraggableProps["style"],
-    index?: number,
-  ) => {
-    if (!style || !style.transform) return style
-
-    const currentIndex = index ?? 0
-
-    const listContainer = document.querySelector(
-      ".list-container",
-    ) as HTMLElement
-    if (!listContainer) return style
-
-    const containerHeight = 334
-    const itemHeight = 54
-    const itemSpacing = 16
-    const containerTop = -(itemHeight + itemSpacing) * currentIndex
-    const currentTransformMatch = style.transform.match(
-      /translate\((-?\d+)px,\s+(-?\d+)px\)/,
-    )
-    if (currentTransformMatch) {
-      const yTransform = parseInt(currentTransformMatch[2])
-      const maxY = containerTop + containerHeight - itemHeight
-      const adjustedY = Math.min(Math.max(yTransform, containerTop), maxY)
-
-      return {
-        ...style,
-        transform: `translate(0px, ${adjustedY}px)`,
-      }
-    }
-
-    return style
   }
 
   return (
@@ -118,50 +63,25 @@ const RoutineFixModal = () => {
       <Modal.Content>
         <S.RoutineFixForm onSubmit={handleSubmit(onSubmit)}>
           <S.MyRoutineWrapper>
-            <DragDropContext onDragEnd={handleDrop}>
-              <Droppable
-                droppableId="routineList"
-                direction="vertical">
-                {(provided) => (
-                  <div
-                    className="list-container"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{ position: "relative" }}>
-                    <S.MyRoutineList>
-                      {watchedRoutines.map((routine, index) => (
-                        <Draggable
-                          key={routine.routineId}
-                          draggableId={`${routine.routineId}`}
-                          index={index}>
-                          {(provided) => {
-                            return (
-                              <RoutineItem
-                                routine={routine}
-                                index={index}
-                                setValue={setValue}
-                                register={register}
-                                provided={{
-                                  ...provided,
-                                  draggableProps: {
-                                    ...provided.draggableProps,
-                                    style: getItemStyle(
-                                      provided.draggableProps.style,
-                                      index,
-                                    ),
-                                  },
-                                }}
-                              />
-                            )
-                          }}
-                        </Draggable>
-                      ))}
-                      {provided.placeholder}
-                    </S.MyRoutineList>
-                  </div>
+            <S.MyRoutineList>
+              <SortableList
+                data={watchedRoutines}
+                renderItem={(item: MyRoutines, index: number) => (
+                  <RoutineItem
+                    routine={item}
+                    index={index}
+                    register={register}
+                    setValue={setValue}
+                    startDrag={(dragIndex) =>
+                      alert(`Dragging started at index ${dragIndex}`)
+                    }
+                  />
                 )}
-              </Droppable>
-            </DragDropContext>
+                onClickItem={(index: number) =>
+                  alert(`Clicked item at index ${index}`)
+                }
+              />
+            </S.MyRoutineList>
           </S.MyRoutineWrapper>
           <Modal.Footer>
             <Button
