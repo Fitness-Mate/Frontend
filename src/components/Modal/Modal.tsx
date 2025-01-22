05a876be8120a4d8b8ab8a6cf8a21c445cbb9040
강민정 <staccato189@gmail.com>
2025-01-22 17:08:45 +0900

    [Fix 🔨] dragPreview 위치 이슈로 인한 모달 컴포넌트 수정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -15,6 +15,7 @@ interface ModalMainProps {
   isOpen: boolean
   onClose: () => void
   disableInteraction?: boolean
+  $useFlexCenter?: boolean
 }
 
 const ModalMain = ({
@@ -23,6 +24,7 @@ const ModalMain = ({
   isOpen,
   onClose,
   disableInteraction = false,
+  $useFlexCenter = false,
 }: PropsWithChildren<ModalMainProps>) => {
   const handleWrapperClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget && !disableInteraction) {
@@ -35,8 +37,12 @@ const ModalMain = ({
   }
 
   return createPortal(
-    <BackOverlay onClick={handleWrapperClick}>
-      <ModalWrapper $isCloseButton={isCloseButton}>
+    <BackOverlay
+      onClick={handleWrapperClick}
+      $useFlexCenter={$useFlexCenter}>
+      <ModalWrapper
+        $isCloseButton={isCloseButton}
+        $useFlexCenter={$useFlexCenter}>
         {isCloseButton && (
           <IconButton
             icon="CloseBold"
@@ -59,31 +65,48 @@ const Modal = Object.assign(ModalMain, {
 
 export default Modal
 
-const BackOverlay = styled.div`
+const BackOverlay = styled.div<{ $useFlexCenter?: boolean }>`
   position: fixed;
   width: 100%;
   height: 100%;
   top: 0;
-  bottom: 0;
   left: 0;
-  right: 0;
   background: #00000033;
   z-index: 1000;
 `
 
-const ModalWrapper = styled.div<{ $isCloseButton: boolean }>`
+const ModalWrapper = styled.div<{
+  $isCloseButton: boolean
+  $useFlexCenter?: boolean
+}>`
   display: flex;
   min-width: 480px;
   padding: 24px;
   padding-top: ${({ $isCloseButton }) => ($isCloseButton ? "48px" : "24px")};
   flex-direction: column;
   align-items: flex-start;
-  gap: 24px;
   border-radius: 20px;
   position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  ${({ $useFlexCenter }) =>
+    $useFlexCenter
+      ? `
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+		width: 420px;
+		min-width: 420px;
+    height: 560px;
+		gap: 0;
+  `
+      : `
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+		gap: 24px;
+		padding: 24px;
+  `}
   background: ${theme.Netural0};
   box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.12);
 `
